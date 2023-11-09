cdf4b3cf1eeb8ee9ccff3eb4e085e17c2efe3c8a
RafalTimbo308 <rafatimbo0308@gmail.com>
2023-11-08 22:26:03 -0300

    configurações iniciais do django / criação de App Ticker

--- B3Tracker/settings.py
+++ B3Tracker/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'Ticker',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -121,3 +122,16 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
+#Configurações para envio de e-mail
+
+EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_PORT =587
+EMAIL_HOST_USER= 'ticker.monitor2023@gmail.com'
+EMAIL_HOST_PASSWORD= 'ffvuqkukrpiaobhz'
+EMAIL_USE_TLS=True
