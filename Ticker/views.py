c6a681bc7856295f5617149ccc493e0d4e3b8622
RafalTimbo308 <rafatimbo0308@gmail.com>
2023-11-09 15:23:02 -0300

    Adicionado tratamento de erros para código de ação inválido

--- Ticker/views.py
+++ Ticker/views.py
@@ -10,24 +10,30 @@ def Home(request):
 
     if request.method =="POST":
     
-        ticker = request.POST.get('ticker')+".SA"
+        ticker = request.POST.get('ticker').upper()+".SA"
         tunnel_inf = request.POST.get('tunnel-inf')
         tunnel_sup = request.POST.get('tunnel-sup')
         now = timezone.now()
-        value = yf.Ticker(ticker).history(period="1d")['Close'].iloc[-1]
         interval = request.POST.get('interval')
+        try:
+            value = yf.Ticker(ticker).history(period="1d")['Close'].iloc[-1]
+            new_obj = Ticker(
+                ticker = ticker,
+                tunnel_inf = tunnel_inf,
+                tunnel_sup = tunnel_sup,
+                value = value,
+                interval = interval,
+                last_update = now)  
+            new_obj.save()            
         
-        new_obj = Ticker(
-            ticker = ticker,
-            tunnel_inf = tunnel_inf,
-            tunnel_sup = tunnel_sup,
-            value = value,
-            interval = interval,
-            last_update = now
-        )
+        except:
+            tickers = Ticker.objects.all()
+            error_msg = f"Ação {ticker} não encontrada."
+            return render(request , "Ticker/home.html",{
+                "tickers":tickers,
+                "error":error_msg,
+            })
         
-        new_obj.save()
-            
     tickers = Ticker.objects.all()
     
     return render(request , "Ticker/home.html", {
