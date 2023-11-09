1e7e3505fac79daa11a645a5ef09f84337df97e0
RafalTimbo308 <rafatimbo0308@gmail.com>
2023-11-08 22:48:50 -0300

    criação de model e configuração de interface de admin

--- Ticker/admin.py
+++ Ticker/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Ticker
 # Register your models here.
+
+class TickerAdmin(admin.ModelAdmin):
+    
+    list_display = ('ticker','value')
+    
+admin.site.register(Ticker,TickerAdmin)
--- Ticker/models.py
+++ Ticker/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Ticker(models.Model):
+    
+    ticker = models.CharField(primary_key=True , max_length=8)
+    tunnel_inf = models.DecimalField(decimal_places=2 , max_digits=6)
+    tunnel_sup = models.DecimalField(decimal_places=2 , max_digits=6)
+    value = models.DecimalField(decimal_places=2 , max_digits=6)
+    last_update = models.DateTimeField()
